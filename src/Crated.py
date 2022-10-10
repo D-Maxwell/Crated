26b08abad2a686aa19a3e6c604b93bab7780ca90
Maxwell <maxim.dovergne@gmail.com>
2022-10-10 20:57:03 +0200

    Added support for arguments, pretty compact, will probably be cleaned ever so slightly in the near future (like never)

--- src/Crated.py
+++ src/Crated.py
@@ -43,11 +43,8 @@ def ship(node:Cargo):
 
                 #v# "rollback" search # to be done dynamically (dicts, strings as var pointers, ...)
 
-                #if text[line][idx] == '.': print("HEY2",marker, idx, text[line][idx])
-
                 if marker == indent*4 and text[line][marker] not in ['#','.','('] and text[line][idx] in ['#','.','(']:
                     tag = text[line][marker : idx]
-                    print("HEY",marker,idx, text[line][marker])
 
                 if text[line][marker] == '#' and text[line][idx] in ['#','.','(']:
                     id = text[line][marker + 1 : idx]
@@ -56,18 +53,37 @@ def ship(node:Cargo):
                     class_ = text[line][marker + 1 : idx]
 
                 if text[line][marker] in ['(',','] and text[line][idx] in [',',')']:
-                    data[text[line][marker + 1 : marker_arg]] = text[line][marker_arg : idx]
+                    if marker_arg == 0: marker_arg = marker
+                    data[text[line][marker + 1 : marker_arg]] = text[line][marker_arg + 1 : idx]
+                    #print(marker, marker_arg)
+
+
+
+
+                # if text[line][marker] == '(' and text[line][idx] == ',':
 
 
 
                 #v# set marker onto any special character
 
-                if idx == indent*4 or text[line][idx] == '#' or text[line][idx] == '.' or text[line][idx] == '(':
+                if idx == indent*4 or text[line][idx] in ['#','.','('] or (text[line][idx] == ',' and text[line][0 : idx].count('[') == text[line][0 : idx].count(']')):
                     marker = idx
                     #print("m '",marker, "' idx '",idx, "' line '", line, "'", sep='')
-                if text[line][marker] == '=':
+
+                if text[line][idx] == '=':
                     marker_arg = idx
 
+                # arg, val = "", ""
+                # if text[line][idx] == '=':
+                #     arg = text[line][marker : idx]
+                #     #data_idx = idx + 1
+                #
+                # # equal amount of brackets not to catch unwanted commas
+                # if text[line][idx] in [',',')'] and text[line][0 : idx].count('[') == text[line][0 : idx].count(']'):
+                #     val = text[line][marker_arg : idx]
+                #     #data_idx = idx + 1
+                #     data[arg] = val
+                #     arg, val = "", ""
 
                 # if text[line][idx] in ['#','.','(']:
                 #     if (tag == "" or tag is None) and text[line][idx] == '#':
@@ -77,11 +93,11 @@ def ship(node:Cargo):
                 #         print(tag, idx, text[line][idx])
                 #     if (class_ == "" or class_ is None) and text[line][idx] == '(':
                 #         class_ = text[line][indent*4+marker : idx]
-
-                #marker = 0 if (tag == "" or tag is None) else idx if text[line][idx] in ['#','.'] else None
+                #
+                # marker = 0 if (tag == "" or tag is None) else idx if text[line][idx] in ['#','.'] else None
                 # marker = idx if text[line][idx] in ['#','.'] else marker
-
-
+                #
+                #
                 # if id == "" or id is None:
                 #     if text[line][idx] == '#':
                 #         marker = idx
@@ -91,21 +107,13 @@ def ship(node:Cargo):
                 #         print('/','>'*indent, id, sep='')
                 #         marker = None
                 # else:
-                #     if text[line][idx] == '=':
-                #         arg = text[line][data_idx : idx]
-                #         data_idx = idx + 1
-                #
-                #     # equal amount of brackets not to catch unwanted commas
-                #     if text[line][idx] in [',',')'] and text[line][0 : idx].count('[') == text[line][0 : idx].count(']'):
-                #         val = text[line][data_idx : idx]
-                #         data_idx = idx + 1
-                #         data[arg] = val
-                #         arg, val = "", ""
+
 
                 # reached End of Line
+
                 if idx == len(text[line])-1:
                     cargo.append(Crate(tag,id,class_,pos=data.get("pos"),size=data.get("size"),children=children))
-                    print("tag",tag,"id",id,"class",class_)
+                    print("tag",tag,"id",id,"class",class_,"data",data)
 
     except FileNotFoundError:
         log('ERROR', "[ERROR] ", "File at path '", node.data, "' could not be found. Make sure to include the '.frg' suffix, and check the scope.")
