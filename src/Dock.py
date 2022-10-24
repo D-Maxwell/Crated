6ec4483261dc84f4093a8519d28d5c32a0c49e95
Maxwell <maxim.dovergne@gmail.com>
2022-10-24 17:16:07 +0200

    Woops meant to overwrite not merge

--- src/Dock.py
+++
@@ -1,9 +0,0 @@
-from Node import Node
-
-class Dock(Node):
-    def __init__(self,id:str=None,data=None,children:list=None):
-        super().__init__(id,data,children)
-
-    def ship(self):
-        for cargo in self.children:
-            cargo.load()
--- src/freight01.frg
+++
@@ -1,8 +0,0 @@
-a#id(pos=[12,12],size=[16,12])
-    ht#abbb(size=[8,8])
-bruh#b()
-        .group1()
-        #id2()
-    idoubtthiswillwork.yesitwill()
-    ()
-wait#did.i(just=make,an=actual,language=myself??)
